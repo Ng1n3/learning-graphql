02d3aa8fb9ccaa75bb43009d2cc1ef90a419f84c
ng1n3 <code.with.muyiwa@gmail.com>
2024-08-11 17:08:54 +0100

    feat: related data

--- _db.js
+++ _db.js
@@ -13,24 +13,24 @@ let authors = [
 ];
 
 let reviews = [
-  { id: '1', rating: 9, content: 'Lorem ipsum', author_id: '1', gamed_id: '2' },
+  { id: '1', rating: 9, content: 'Lorem ipsum', author_id: '1', game_id: '2' },
   {
     id: '2',
     rating: 10,
     content: 'Lorem ipsum',
     author_id: '2',
-    gamed_id: '1',
+    game_id: '1',
   },
-  { id: '3', rating: 7, content: 'Lorem ipsum', author_id: '3', gamed_id: '3' },
-  { id: '4', rating: 5, content: 'Lorem ipsum', author_id: '2', gamed_id: '4' },
-  { id: '5', rating: 9, content: 'Lorem ipsum', author_id: '2', gamed_id: '5' },
-  { id: '6', rating: 7, content: 'Lorem ipsum', author_id: '1', gamed_id: '2' },
+  { id: '3', rating: 7, content: 'Lorem ipsum', author_id: '3', game_id: '3' },
+  { id: '4', rating: 5, content: 'Lorem ipsum', author_id: '2', game_id: '4' },
+  { id: '5', rating: 9, content: 'Lorem ipsum', author_id: '2', game_id: '5' },
+  { id: '6', rating: 7, content: 'Lorem ipsum', author_id: '1', game_id: '2' },
   {
     id: '7',
     rating: 10,
     content: 'Lorem ipsum',
     author_id: '3',
-    gamed_id: '1',
+    game_id: '1',
   },
 ];
 
--- index.js
+++ index.js
@@ -15,12 +15,33 @@ const resolvers = {
     authors() {
       return db.authors;
     },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
     review(_, args) {
       return db.reviews.find((review) => review.id === args.id);
     },
     author(_, args) {
-      return db.authors.find((author) => author.id === args.id)
-    }
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
   },
 };
 
