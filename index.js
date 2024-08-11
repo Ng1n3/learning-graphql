3a52677075362306cdaf1e6f1c3eb3b447083c71
ng1n3 <code.with.muyiwa@gmail.com>
2024-08-11 16:23:27 +0100

    feat: adding resolvers

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-import { typeDefs } from "./schema";
+import db from './_db.js';
+import { typeDefs } from './schema.js';
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
+
+//typeDefs -- definitions of types of data to expose on our graph
+//resolvers
 
 // server setup
 const server = new ApolloServer({
-  //typeDefs -- definitions of types of data to expose on our graph
   typeDefs,
-  //resolvers
-})
+  resolvers
+});
 
-const {url} = await startStandaloneServer(server, {
-  listen: {port: 4000}
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-console.log('Server ready at port', 4000)
+console.log('Server ready at port', 4000);
