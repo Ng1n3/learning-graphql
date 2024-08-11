b7e06b6bc7af09b9aedac5fd8a4a1cb6de596380
ng1n3 <code.with.muyiwa@gmail.com>
2024-08-11 15:41:21 +0100

    feat: create fake database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+import { typeDefs } from "./schema";
+
 // server setup
 const server = new ApolloServer({
   //typeDefs -- definitions of types of data to expose on our graph
+  typeDefs,
   //resolvers
 })
 
